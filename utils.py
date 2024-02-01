1e3527d22edbf52805f67a7c1eab9885574b2ee9
Kahsolt <kahsolt@qq.com>
2024-02-01 18:56:38 +0800

    add data preprocess

---
+++ utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Armit
+# Create Time: 2024/02/01
+
+from pathlib import Path
+from typing import *
+
+import numpy as np
+from numpy import ndarray
+from tqdm import tqdm
+
+BASE_PATH = Path(__file__).parent
+DATA_PATH = BASE_PATH / 'data'
